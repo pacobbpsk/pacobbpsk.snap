aa6e78cbe5d6f20cfceb11011ff353b7b07cec7b
pacobbpsk <66210873+pacobbpsk@users.noreply.github.com>
2020-08-16 02:07:32 +0900

    Create sneaker.js

---
+++ sneaker.js
@@ -0,0 +1,14 @@
+$(function(){
+
+  $('#menu-icon').click(function(){
+    $('#grav').fadeIn();
+   });
+  $('.close-btn').click(function(){
+    $('#grav').fadeOut();
+  });
+ 
+    
+  $('.list').click(function(){
+    $('#grav').fadeOut();
+  });
+});
